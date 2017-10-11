d59d6f1a365817758d26c0a02521c7b9afd22f26
Goular <zhaojt_cs@126.com>
2017-10-11 22:08:43 +0800

    添加验证码的插件

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //2017-10-11 添加验证码 mewebstudio/captcha
+        Mews\Captcha\CaptchaServiceProvider::class,
     ],
 
     /*
@@ -226,6 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        //2017-10-11 添加验证码 mewebstudio/captcha
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
     ],
 
 ];
