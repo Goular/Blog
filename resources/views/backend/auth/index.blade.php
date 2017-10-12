89266dc61eee132e3a5c9cd385db0f42c0bd4d81
Goular <zhaojt_cs@126.com>
2017-10-12 11:41:58 +0800

    提交内容

--- resources/views/backend/auth/index.blade.php
+++ resources/views/backend/auth/index.blade.php
@@ -1,39 +1,96 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="{{asset('/admin-ui/css/ch-ui.admin.css')}}">
-  <link rel="stylesheet" href="{{asset('/admin-ui/font/css/font-awesome.min.css')}}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 2 | Log in</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{url('adminlte/css/AdminLTE.min.css')}}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{url('adminlte/plugins/iCheck/square/blue.css')}}">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body style="background:#F3F3F4;">
-<div class="login_box">
-  <h1>Blog</h1>
-  <h2>欢迎使用博客管理平台</h2>
-  <div class="form">
-    <form action="#" method="post">
-      {{csrf_field()}}
-      <ul>
-        <li>
-          <input type="text" name="username" class="text"/>
-          <span><i class="fa fa-user"></i></span>
-        </li>
-        <li>
-          <input type="password" name="password" class="text"/>
-          <span><i class="fa fa-lock"></i></span>
-        </li>
-        <li>
-          <input type="text" class="code" name="captcha"/>
-          <span><i class="fa fa-check-square-o"></i></span>
-          <img src="{{url('captcha')}}" alt="" onclick="this.src='{{url('captcha')}}?'+Math.random()">
-        </li>
-        @include('backend.layout.errorMsg')
-        <li>
-          <input type="submit" value="立即登陆"/>
-        </li>
-      </ul>
-    </form>
-    <p><a href="{{url("")}}">返回首页</a> &copy; 2016 Powered by Goular <a href="{{url("")}}" target="_blank">{{url("")}}</a></p>
-  </div>
+<body class="hold-transition login-page" style="background:url(/images/bg-admin.jpg) repeat;">
+<div class="login-box">
+    <div class="login-logo">
+        <a href="/adminlte/index2.html"><b>加工屋</b>后台系统</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">登录</p>
+
+        <form action="/adminlte/index2.html" method="post">
+            <div class="form-group has-feedback">
+                <input type="email" class="form-control" placeholder="Email">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Password">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <div class="col-xs-8">
+                    <div class="checkbox icheck">
+                        <label>
+                            <input type="checkbox"> Remember Me
+                        </label>
+                    </div>
+                </div>
+                <!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
+
+        <div class="social-auth-links text-center">
+            <p>- OR -</p>
+            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in
+                using
+                Facebook</a>
+            <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in
+                using
+                Google+</a>
+        </div>
+        <!-- /.social-auth-links -->
+
+        <a href="#">I forgot my password</a><br>
+        <a href="register.html" class="text-center">Register a new membership</a>
+
+    </div>
+    <!-- /.login-box-body -->
 </div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.2.3 -->
+<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="{{url('adminlte/plugins/iCheck/icheck.min.js')}}"></script>
+<script>
+    $(function () {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+    });
+</script>
 </body>
 </html>
