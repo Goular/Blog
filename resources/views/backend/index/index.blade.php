73947cfbab9e7bf5c5b7c12e5a51b47b60216dae
Goular <zhaojt_cs@126.com>
2017-10-12 16:49:39 +0800

    添加组件的内容的文件夹

--- resources/views/backend/index/index.blade.php
+++ resources/views/backend/index/index.blade.php
@@ -3,7 +3,7 @@
 @section("page-title","后台管理系统--首页")
 
 @section('page-content')
-的算法第三方第三方发的所发生的防守打法
+
 @endsection
 
 @section('page-js')
--- resources/views/backend/layout/main.blade.php
+++ resources/views/backend/layout/main.blade.php
@@ -499,7 +499,23 @@
     </aside>
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">@yield('page-content')</div>
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Dashboard
+                <small>Control panel</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+            @yield('page-content')
+        </section>
+    </div>
 
     <!-- /.content-wrapper -->
     <footer class="main-footer">
