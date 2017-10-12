b922a2cdabe363bb513e795ac76c6d3f06b3cc81
Goular <zhaojt_cs@126.com>
2017-10-12 11:57:20 +0800

    界面修改

--- resources/views/backend/auth/index.blade.php
+++ resources/views/backend/auth/index.blade.php
@@ -27,7 +27,7 @@
 <body class="hold-transition login-page" style="background:url({{url('images/bg-admin.svg')}}) no-repeat center;background-size: 100%">
 <div class="login-box">
     <div class="login-logo">
-        <a href="/adminlte/index2.html"><b>加工屋</b>后台系统</a>
+        <a href="/adminlte/index2.html"><b>博客</b>后台系统</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
@@ -35,42 +35,42 @@
 
         <form action="/adminlte/index2.html" method="post">
             <div class="form-group has-feedback">
-                <input type="email" class="form-control" placeholder="Email">
+                <input type="email" class="form-control" placeholder="请输入用户名">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Password">
+                <input type="password" class="form-control" placeholder="请输入密码">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
                 <div class="col-xs-8">
                     <div class="checkbox icheck">
                         <label>
-                            <input type="checkbox"> Remember Me
+                            <input type="checkbox"> 记住我
                         </label>
                     </div>
                 </div>
                 <!-- /.col -->
                 <div class="col-xs-4">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button>
                 </div>
                 <!-- /.col -->
             </div>
         </form>
 
-        <div class="social-auth-links text-center">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in
-                using
-                Facebook</a>
-            <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in
-                using
-                Google+</a>
-        </div>
+        {{--<div class="social-auth-links text-center">--}}
+            {{--<p>- 或 -</p>--}}
+            {{--<a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in--}}
+                {{--using--}}
+                {{--Facebook</a>--}}
+            {{--<a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in--}}
+                {{--using--}}
+                {{--Google+</a>--}}
+        {{--</div>--}}
         <!-- /.social-auth-links -->
 
-        <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="#">忘记密码?</a><br>
+        <a href="register.html" class="text-center">注册会员</a>
 
     </div>
     <!-- /.login-box-body -->
