953a2566057da9710ebde49e6edcdc871b889292
Goular <zhaojt_cs@126.com>
2017-10-12 16:18:40 +0800

    提交界面的内容

--- resources/views/backend/layout/main.blade.php
+++ resources/views/backend/layout/main.blade.php
@@ -506,8 +506,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.0
         </div>
-        <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-        reserved.
+        <strong>Copyright &copy; 2016-2018 <a href="{{url('')}}">加工屋</a> @</strong> 版权所有.
     </footer>
 
     <!-- Control Sidebar -->
