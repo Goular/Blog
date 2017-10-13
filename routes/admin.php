3027a5f66881307a147074b5da2bf42b6fb33c7c
Goular <zhaojt_cs@126.com>
2017-10-14 01:16:37 +0800

    添加目录类的控制器

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,13 @@
 <?php
 //网站后台的路由配置文件
 Route::prefix('admin')->group(function () {
-    Route::get('/login', '\App\Admin\Controllers\AuthController@index');
-    Route::post('/login', '\App\Admin\Controllers\AuthController@login');
+    Route::get('login', '\App\Admin\Controllers\AuthController@index');
+    Route::post('login', '\App\Admin\Controllers\AuthController@login');
     Route::middleware(['admin.identify'])->group(function () {
         Route::get('/', '\App\Admin\Controllers\IndexController@index');
-        Route::get('/logout', '\App\Admin\Controllers\AuthController@logout');
-        Route::get('/changepwd', '\App\Admin\Controllers\AuthController@changePwdIndex');
-        Route::post('/changepwd', '\App\Admin\Controllers\AuthController@changePwd');
+        Route::get('logout', '\App\Admin\Controllers\AuthController@logout');
+        Route::get('changepwd', '\App\Admin\Controllers\AuthController@changePwdIndex');
+        Route::post('changepwd', '\App\Admin\Controllers\AuthController@changePwd');
+        Route::resource('category', '\App\Admin\Controllers\CategoryController');
     });
 });
