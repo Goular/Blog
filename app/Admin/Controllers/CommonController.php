d5f30fc128e1e35f946470ec04e3ad361eda4e2b
Goular <zhaojt_cs@126.com>
2017-10-15 16:38:54 +0800

    提交修改的内容

--- app/Admin/Controllers/CommonController.php
+++ app/Admin/Controllers/CommonController.php
@@ -11,4 +11,25 @@ use \App\Http\Controllers\Controller;
  */
 class CommonController extends Controller
 {
+    /**
+     * 将数据按照下面的JSON格式进行输出
+     * @param $data
+     * @param $status
+     * @param $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function toJson($data, $status, $message)
+    {
+        return response()->json(compact('status', 'message', 'data'));
+    }
+
+    /**
+     * Ajax请求，将表单验证器的错误信息进行归集成一条字符串数据
+     * @param $validator    手动表单验证器执行完成后返回的对象
+     * @param string $guleFormat implode方法的合并错误信息的间隔符号
+     */
+    public function compactAjaxErrorsMsg($validator, $guleFormat = ',')
+    {
+
+    }
 }
