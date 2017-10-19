37287f9a8d5aabb60a7847e9e3ebd35d65c303bb
Goular <zhaojt_cs@126.com>
2017-10-19 09:42:56 +0800

    提交没有定义的数据库迁移和创建相关的ORM实体

---
+++ app/Entities/WebConfig.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WebConfig extends Model
+{
+    protected $guarded = [];
+    //protected $fillable = [];
+    //protected $hidden = [];
+}
