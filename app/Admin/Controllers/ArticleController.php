a7fea13803b0cead47f82e1bc6cf72445be3226a
Goular <zhaojt_cs@126.com>
2017-10-19 09:32:39 +0800

    提交后台相关的控制器

---
+++ app/Admin/Controllers/ArticleController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * 博客文章使用的控制器
+ * Class ArticleController
+ * @package App\Admin\Controllers
+ */
+class ArticleController extends CommonController
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+}
---
+++ app/Admin/Controllers/FriendLinkController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * 友情链接控制器
+ * Class FriendLinkController
+ * @package App\Admin\Controllers
+ */
+class FriendLinkController extends CommonController
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+}
