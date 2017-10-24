0221b6a3fb83c54b9a6d56fe5fbdda6fbbf4f5bd
Goular <zhaojt_cs@126.com>
2017-10-24 16:52:30 +0800

    提交文章的数据库迁移

--- database/migrations/2017_10_19_093536_create_articles_table.php
+++ database/migrations/2017_10_19_093536_create_articles_table.php
@@ -15,6 +15,14 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("title")->unique()->comment("文章标题");
+            $table->string("tag")->nullable()->comment("关键词");
+            $table->string("description")->nullable()->comment("文章描述");
+            $table->string("thumb")->nullable()->comment("文章缩略图");
+            $table->text("content")->nullable()->comment("文章正文");
+            $table->string("editor")->nullable()->comment("作者");
+            $table->string("view")->nullable()->comment("查看次数");
+            $table->string("cate_id")->nullable()->comment("标题");
             $table->timestamps();
             $table->softDeletes();
         });
