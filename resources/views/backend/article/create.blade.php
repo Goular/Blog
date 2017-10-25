a615807747231ee42ff74f3dee686d5b90518e67
Goular <zhaojt_cs@126.com>
2017-10-26 00:14:01 +0800

    提交ueditor

--- resources/views/backend/article/create.blade.php
+++ resources/views/backend/article/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('page-content')
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             <!-- Horizontal Form -->
             <div class="box box-success">
                 <div class="box-header with-border">
@@ -16,8 +16,8 @@
                     {{csrf_field()}}
                     <div class="box-body">
                         <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-label">父级分类</label>
-                            <div class="col-sm-10">
+                            <label for="inputEmail3" class="col-sm-1 control-label">文章分类</label>
+                            <div class="col-sm-11">
                                 <select name="parent_id" class="form-control select2" style="width: 100%;">
                                     <option value="0">==顶级分类==</option>
                                     @foreach($categories as $category)
@@ -27,35 +27,44 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="name" class="col-sm-2 control-label">分类名称<label
+                            <label for="name" class="col-sm-1 control-label">文章标题<label
                                         class="text-red">(必填)</label></label>
-                            <div class="col-sm-10">
-                                <input name="name" type="text" class="form-control" id="name" placeholder="请输入分类名称">
+                            <div class="col-sm-11">
+                                <input name="name" type="text" class="form-control" id="name" placeholder="请输入文章标题"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="title" class="col-sm-2 control-label">分类标题<label
-                                        class="text-red">(必填)</label></label>
-                            <div class="col-sm-10">
-                                <input name="title" type="text" class="form-control" id="title" placeholder="请输入分类标题">
+                            <label for="title" class="col-sm-1 control-label">编辑者</label>
+                            <div class="col-sm-11">
+                                <input name="title" type="text" class="form-control" id="title" placeholder="请输入编辑者名称"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="keywords" class="col-sm-2 control-label">关键词</label>
-                            <div class="col-sm-10">
-                                <textarea name="keywords" class="form-control" id="keywords" rows="3" placeholder="请输入关键词"></textarea>
+                            <label for="keywords" class="col-sm-1 control-label">缩略图</label>
+                            <div class="col-sm-11">
+                                <input name="title" type="text" class="form-control" id="description" rows="3"
+                                       placeholder="请输入关键词"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="description" class="col-sm-2 control-label">描述</label>
-                            <div class="col-sm-10">
-                                <textarea name="description" class="form-control" id="description" rows="3" placeholder="请输入描述"></textarea>
+                            <label for="description" class="col-sm-1 control-label">关键词</label>
+                            <div class="col-sm-11">
+                                <input name="title" type="text" class="form-control" id="description" rows="3"
+                                       placeholder="请输入关键词"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="order" class="col-sm-2 control-label">排序</label>
-                            <div class="col-sm-10">
-                                <input name="order" type="number" class="form-control" id="order" placeholder="请输入排序数值" value="0">
+                            <label for="keywords" class="col-sm-1 control-label">正文</label>
+                            <div class="col-sm-11">
+                                <textarea name="keywords" class="form-control" id="keywords" rows="3"
+                                          placeholder="请输入正文"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="order" class="col-sm-1 control-label">描述</label>
+                            <div class="col-sm-11">
+                                <input name="order" type="number" class="form-control" id="order" placeholder="请输入排序数值"
+                                       value="0">
                             </div>
                         </div>
                         @include("backend.layout.errorMsg")
