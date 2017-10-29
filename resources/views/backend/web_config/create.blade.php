4ee39b8662915b1a3fc5c7c3b153f64071aba0fd
Goular <zhaojt_cs@126.com>
2017-10-29 15:32:29 +0800

    提交内容

--- resources/views/backend/web_config/create.blade.php
+++ resources/views/backend/web_config/create.blade.php
@@ -4,11 +4,11 @@
 
 @section('page-content')
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             <!-- Horizontal Form -->
             <div class="box box-success">
                 <div class="box-header with-border">
-                    <h3 class="box-title">友情链接添加</h3>
+                    <h3 class="box-title">网站配置添加</h3>
                 </div>
                 <!-- /.box-header -->
                 <!-- form start -->
@@ -16,21 +16,14 @@
                     {{csrf_field()}}
                     <div class="box-body">
                         <div class="form-group">
-                            <label for="title" class="col-sm-2 control-label">标题<label
+                            <label for="title" class="col-sm-2 control-label">配置标题<label
                                         class="text-red">(必填)</label></label>
                             <div class="col-sm-10">
-                                <input name="title" type="text" class="form-control" id="title" placeholder="请输入标题" value="{{old('title')}}">
+                                <input name="title" type="text" class="form-control" id="title" placeholder="请输入配置标题" value="{{old('title')}}">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="name" class="col-sm-2 control-label">名称<label
-                                        class="text-red">(必填)</label></label>
-                            <div class="col-sm-10">
-                                <input name="name" type="text" class="form-control" id="name" placeholder="请输入变量名" value="{{old('name')}}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="type" class="col-sm-2 control-label">类型</label>
+                            <label for="type" class="col-sm-2 control-label">配置类型</label>
                             <div class="col-sm-10">
                                 <label class="radio-inline">
                                     <input type="radio" name="type" id="inlineRadio1" value="input" checked onclick="showTr()"> input
@@ -44,21 +37,28 @@
                             </div>
                         </div>
                         <div class="form-group value">
-                            <label for="value" class="col-sm-2 control-label">类型值</label>
+                            <label for="value" class="col-sm-2 control-label">配置类型值</label>
                             <div class="col-sm-10">
-                                <input name="value" type="number" class="form-control" id="value" placeholder="请输入选项值，用逗号 ',' 进行分隔." value="{{old('value')??''}}">
+                                <input name="value" type="number" class="form-control" id="value" placeholder="格式: 1|开启,0|关闭" value="{{old('value')??''}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">配置名称[英文]<label
+                                        class="text-red">(必填)</label></label>
+                            <div class="col-sm-10">
+                                <input name="name" type="text" class="form-control" id="name" placeholder="请输入配置名称" value="{{old('name')}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="tips" class="col-sm-2 control-label">配置值</label>
+                            <div class="col-sm-10">
+                                <textarea name="tips" class="form-control" id="description" rows="3" placeholder="请输入配置值">{{old('tips')}}</textarea>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="order" class="col-sm-2 control-label">排序</label>
                             <div class="col-sm-10">
                                 <input name="order" type="number" class="form-control" id="order" placeholder="请输入排序" value="{{old('order')}}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="tips" class="col-sm-2 control-label">描述</label>
-                            <div class="col-sm-10">
-                                <textarea name="tips" class="form-control" id="description" rows="3" placeholder="请输入说明">{{old('tips')}}</textarea>
                             </div>
                         </div>
                         @include("backend.layout.errorMsg")
--- resources/views/backend/web_config/update.blade.php
+++ resources/views/backend/web_config/update.blade.php
@@ -4,11 +4,11 @@
 
 @section('page-content')
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             <!-- Horizontal Form -->
             <div class="box box-success">
                 <div class="box-header with-border">
-                    <h3 class="box-title">友情链接添加</h3>
+                    <h3 class="box-title">网站配置添加</h3>
                 </div>
                 <!-- /.box-header -->
                 <!-- form start -->
@@ -17,21 +17,14 @@
                     {{method_field("put")}}
                     <div class="box-body">
                         <div class="form-group">
-                            <label for="title" class="col-sm-2 control-label">标题<label
+                            <label for="title" class="col-sm-2 control-label">配置标题<label
                                         class="text-red">(必填)</label></label>
                             <div class="col-sm-10">
                                 <input name="title" type="text" class="form-control" id="title" placeholder="请输入标题" value="{{$selectConfig->title}}">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="name" class="col-sm-2 control-label">名称<label
-                                        class="text-red">(必填)</label></label>
-                            <div class="col-sm-10">
-                                <input name="name" type="text" class="form-control" id="name" placeholder="请输入变量名" value="{{$selectConfig->name}}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="type" class="col-sm-2 control-label">类型</label>
+                            <label for="type" class="col-sm-2 control-label">配置类型</label>
                             <div class="col-sm-10">
                                 <label class="radio-inline">
                                     <input type="radio" name="type" id="inlineRadio1" value="input" @if($selectConfig->type == 'input') checked @endif onclick="showTr()"> input
@@ -45,21 +38,28 @@
                             </div>
                         </div>
                         <div class="form-group value">
-                            <label for="value" class="col-sm-2 control-label">类型值</label>
+                            <label for="value" class="col-sm-2 control-label">配置类型值</label>
                             <div class="col-sm-10">
-                                <input name="value" type="number" class="form-control" id="value" placeholder="请输入类型值" value="{{$selectConfig->value}}">
+                                <input name="value" type="number" class="form-control" id="value" placeholder="格式: 1|开启,0|关闭" value="{{$selectConfig->value}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">配置名称[英文]<label
+                                        class="text-red">(必填)</label></label>
+                            <div class="col-sm-10">
+                                <input name="name" type="text" class="form-control" id="name" placeholder="请输入配置名称" value="{{$selectConfig->name}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="tips" class="col-sm-2 control-label">配置值</label>
+                            <div class="col-sm-10">
+                                <textarea name="tips" class="form-control" id="description" rows="3" placeholder="请输入配置值">{{$selectConfig->tips}}</textarea>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="order" class="col-sm-2 control-label">排序</label>
                             <div class="col-sm-10">
                                 <input name="order" type="number" class="form-control" id="order" placeholder="请输入排序" value="{{$selectConfig->order}}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="tips" class="col-sm-2 control-label">描述</label>
-                            <div class="col-sm-10">
-                                <textarea name="tips" class="form-control" id="description" rows="3" placeholder="请输入说明">{{$selectConfig->tips}}</textarea>
                             </div>
                         </div>
                         @include("backend.layout.errorMsg")
