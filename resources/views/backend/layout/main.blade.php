1b28e96572a476dbc2db40c5076b25e8f037400c
Goular <zhaojt_cs@126.com>
2017-10-29 18:51:59 +0800

    提交界面的内容

--- resources/views/backend/layout/main.blade.php
+++ resources/views/backend/layout/main.blade.php
@@ -43,15 +43,7 @@
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <h1>
-                Dashboard
-                <small>Control panel</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="glyphicon glyphicon-home"></i>首页</a></li>
-                <li class="">商品管理</li>
-                <li class="active">添加商品</li>
-            </ol>
+            @yield('page-nav-content')
         </section>
         <!-- Main content -->
         <section class="content">
