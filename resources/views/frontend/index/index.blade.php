c24c2d39ac9a0e919069f2c2cd40802977a524f8
Goular <zhaojt_cs@126.com>
2017-10-30 15:29:08 +0800

    提交内容

--- resources/views/frontend/index/index.blade.php
+++ resources/views/frontend/index/index.blade.php
@@ -10,9 +10,9 @@
     <div class="banner">
         <section class="box">
             <ul class="texts">
-                <p>打了死结的青春，捆死一颗苍白绝望的灵魂。</p>
-                <p>为自己掘一个坟墓来葬心，红尘一梦，不再追寻。</p>
-                <p>加了锁的青春，不会再因谁而推开心门。</p>
+                <p>一只遨游互联网的菜鸟,苦苦追寻技术</p>
+                <p>才发现客户需求凌驾一切</p>
+                <p>知道真相后,只有一句,"去你大爷的!早干嘛去了"</p>
             </ul>
             <div class="avatar"><a href="#"><span>加工屋</span></a></div>
         </section>
